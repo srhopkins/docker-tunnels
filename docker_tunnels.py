d42bc5c8d2ac0f31d5db1dc337696ee0734828c7
Steven Hopkins <shopkins@shutterfly.com>
2015-12-17 17:18:13 -0700

    Using clearer fname syntax

--- docker_tunnels.py
+++ docker_tunnels.py
@@ -58,7 +58,7 @@ def make_script(json_file, scriptname):
 # Do the work
 for json_file in glob.glob("/tunnels/*.json"):
     script, hosts = make_script(json_file, scriptname)
-    fname = '.'.join(os.path.basename(json_file).split('.')[:-1])
+    fname = os.path.splitext(os.path.basename(json_file))[0]
     with open('/tunnels/%s.sh' % fname, 'w') as f:
         f.write(script)
     with open('/etc/hosts.d/%s.hosts' % fname, 'w') as f:
